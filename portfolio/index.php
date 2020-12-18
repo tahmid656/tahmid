736d4648880d7967717e5570c1c603f528fff400
Tahmid Rahman <tahmidrahman.656@gmail.com>
2020-12-19 00:57:44 +0800

    Removed irrelevant lines

--- portfolio/index.php
+++ portfolio/index.php
@@ -29,10 +29,10 @@
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="shortcut icon" href="../assets/thumb.png" type="image/png">
-    <link rel="stylesheet" href="../css/animate.min.css">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css'>
+<!--    <link rel="stylesheet" href="../css/animate.min.css">-->
+<!--    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.1/animate.min.css'>-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">-->
 </head>
 <body>
 <a id="button"></a>
